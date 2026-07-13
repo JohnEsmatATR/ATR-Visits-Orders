da07f56b8092884a3b6b688c433ddcc4232f0a23
John Esmat <john.esmat@atr-eg.com>
2026-07-13 19:04:58 +0300

    Resolve Issues 6, Create New Invoice Fragment

---
+++ app/src/main/java/com/akhnaton/foodvisits/ui/home/order/InvoiceFragment2.kt
@@ -0,0 +1,23 @@
+package com.akhnaton.foodvisits.ui.home.order
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.akhnaton.foodvisits.R
+
+class InvoiceFragment2 : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_invoice2, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
